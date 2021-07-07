987e8ae17416c7b631305f06985cafdf59d23163
Matthew Goad <matthewxgoad@gmail.com>
2021-07-07 18:26:06 -0500

    set up event place trade profile card components

---
+++ client/src/components/eventCard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function eventCard() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default eventCard;
---
+++ client/src/components/profileCard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function profileCard() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default profileCard;
