9e93f34f8f1eb2466ed5f5a3b88c69009a72bf9f
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-07-11 16:10:44 -0500

    messenger page added and route added in App.js. Also hardcoded/formated components to see structure

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Inbox from "./pages/Inbox";
+import Messenger from "./pages/messenger/Messenger";
 import Add from "./pages/Add";
 import Root from "./pages/Root";
 import {
@@ -62,6 +63,11 @@ function App() {
           {/* {user ? <Inbox /> : <SignUp />} */}
           {<Inbox />}
         </Route>
+        <Route path="/messenger">
+          {/* {user ? <Messenger /> : <SignUp />} */}
+          {<Messenger />}
+        </Route>
+
       </Switch>
     </Router>
   );
