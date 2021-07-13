c57858e86de14be6993f91a810976f005b44a91a
Felicia Wootton <felicia.wootton@gmail.com>
2021-07-12 22:27:00 -0500

    sign-up page successfully sends post request to backend

--- client/src/components/SignUpForm/SignUpForm.jsx
+++ client/src/components/SignUpForm/SignUpForm.jsx
@@ -4,10 +4,10 @@ import { Grid,Paper, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 import green from "@material-ui/core/colors/green";
 import Link from "@material-ui/core/Link";
-
+import axios from "axios";
+import { useState } from "react";
 
 const headerColor = green[600];
-
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -35,31 +35,99 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 export default function SignUpForm() {
-    const classes = useStyles();
-    return (
-        <Grid>
-            <Paper elevation={20} className={classes.paper}>
-                <Grid align='center'>
-                    <h2 className={classes.header}>growr</h2>
-                    <h4>Fill out form below to create a new account</h4>
-                </Grid>
-                <form>
-                    <TextField required fullWidth label='Username' placeholder='Create a username'/>
-                    <TextField required fullWidth label='Email' placeholder='Enter your email'/>
-                    <TextField required fullWidth label='Address' placeholder='Enter your address'/>
-                    <TextField type='password' required fullWidth label='Password'placeholder='Create a password'/>
-                    <h5 className={classes.uploadCaption}>Upload a Profile Picture</h5>
-                    <input required accept="image/*" type="file"/>
-                    <br/>
-                    <div className={classes.submitBtn}>
-                        <Button type='submit' variant='contained' color='primary'>Create Account</Button>
-                    </div>
-                </form>
-                <Grid align='center'>
-                    <Link className={classes.link} component={Link} href='/login'>Already have an account? Click here to log in</Link>
-                </Grid>
-            </Paper>
-        </Grid>
-    )
-}
+  
+  const [selectedFile, setSelectedFile] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setPassword] = useState("");
 
+  const classes = useStyles();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('button clicked')
+
+      const user = {
+        name: name,
+        email: email,
+        address: address,
+        password: password,
+        image: selectedFile
+      };
+
+      console.log(user)
+
+      try {
+        console.log('>>>>>making a call<<<<<<<', user)
+        await axios.post("http://localhost:3000/api/users/signup", user);
+      } catch (err) {
+        console.log(err);
+      }
+  };
+
+    const handleNameInput = (event) =>{
+      setName(event.target.value)
+    }
+
+    const handleEmailInput = (event) =>{
+      setEmail(event.target.value)
+    }
+
+    const handleAddressInput = (event) =>{
+      setAddress(event.target.value)
+    }
+
+    const handlePasswordInput = (event) =>{
+      setPassword(event.target.value)
+    }
+
+    const handleFileInput = (event) =>{
+      setSelectedFile(event.target.files[0])
+    }
+
+  return (
+      <Grid>
+          <Paper elevation={20} className={classes.paper}>
+              <Grid align='center'>
+                  <h2 className={classes.header}>growr</h2>
+                  <h4>Fill out form below to create a new account</h4>
+              </Grid>
+              <form onSubmit={handleSubmit}>
+                  <TextField value={name}
+                    onChange={handleNameInput} 
+                    required fullWidth label='Username' placeholder='Create a username'/>
+                  <TextField 
+                    value={email}
+                    onChange={handleEmailInput} 
+                    required fullWidth label='Email' placeholder='Enter your email'/>
+                  <TextField 
+                    value={address}
+                    onChange={handleAddressInput} 
+                    required fullWidth label='Address' placeholder='Enter your address'/>
+                  <TextField 
+                    value={password}
+                    onChange={handlePasswordInput} 
+                    type='password' required fullWidth label='Password'placeholder='Create a password'/>
+                  <h5 className={classes.uploadCaption}>Upload a Profile Picture</h5>
+                  <input 
+                    // value={selectedFile}
+                    onChange={handleFileInput}
+                    encType = 'multipart/form-data'
+                    required accept="image/*" type="file"/> 
+                  <br/>
+                  <div className={classes.submitBtn}>
+                    <Button type='submit' variant='contained' color='primary'>
+                      Create Account
+                    </Button>
+                  </div>
+              </form>
+              <Grid align='center'>
+                  <Link className={classes.link} component={Link} href='/login'>
+                    Already have an account? Click here to log in
+                  </Link>
+              </Grid>
+          </Paper>
+      </Grid>
+  )
+}
