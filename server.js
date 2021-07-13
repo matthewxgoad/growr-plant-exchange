bd1e06dd61414e3be92c378225732c3ce4755961
Felicia Wootton <felicia.wootton@gmail.com>
2021-07-12 22:48:05 -0500

    added app.use(cors()) to server.js

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(cors())
+
 // Static images
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
