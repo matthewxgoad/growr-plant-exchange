4e3012453d92268ee617d9bfb0451e988e2a7fed
Felicia Wootton <felicia.wootton@gmail.com>
2021-07-12 19:19:20 -0500

    commented out fileUpload in router.post for sign up

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup', fileUpload.single('image'),
+router.post('/signup', //fileUpload.single('image'),
   [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
