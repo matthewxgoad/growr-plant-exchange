5a2ff9c1aa2e7ba4cb46d823daadd3c2760fec6c
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-07-14 17:00:46 -0500

    additional and edited material-ui components

--- client/src/components/MessageBox/MessageBox.jsx
+++ client/src/components/MessageBox/MessageBox.jsx
@@ -1,48 +1,99 @@
-import React from 'react';
-import { Grid,Paper, TextField, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
+import { React, useState } from 'react';
+import axios from "axios";
+import { Grid, Paper, TextField, Button } from '@material-ui/core';
+import Divider from "@material-ui/core/Divider";
+import { makeStyles, withStyles } from '@material-ui/core';
+import brown from "@material-ui/core/colors/brown";
 import green from "@material-ui/core/colors/green";
-import Link from "@material-ui/core/Link";
-import { useState } from "react";
 import Message from "../../components/message/Message";
 
 
 const headerColor = green[600];
+const newBrown = brown[600];
+
+const CustomTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: headerColor,
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: newBrown,
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: newBrown,
+      },
+      '&:hover fieldset': {
+        borderColor: newBrown,
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: newBrown,
+      },
+    },
+  },
+})(TextField);
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 2,
-        height: 'auto',
-        margin: "20px"
-      },
-      paper: {
-        padding: theme.spacing(2),
-        marginTop: '10px',
-        textAlign: 'center',
-        height: '80%',
-      },
+  root: {
+      flexGrow: 2,
+      height: 'auto',
+      maxWidth: '68%',
+      margin: "20px"
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      flex: '5.5',
+    },
     header: {
       margin: "0",
       fontFamily: 'Dancing Script',
       color: headerColor,
       fontSize: '35px'
     },
-    uploadCaption: {
-      marginBottom:"0",
+    messageBoxTop: {
+      maxHeight: '60%',
+      overflowY: 'auto',
     },
-    submitBtn: {
-      margin: '20px 0px',
-      textAlign:'center'
+    form: {
+      marginTop: '15px'
     },
-    link: {
-      textDecoration: "none",
-      fontWeight: 'bolder',
+    btnWrap: {
+      margin: '10px 10px 0 0',
+      textAlign: 'right'
     },
+    btn: {
+      backgroundColor: headerColor,
+    }
   }));
 
 export default function MessageBox() {
 
     const classes = useStyles();
+
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log('button clicked')
+  
+        const outgoingMessage = {
+          message: message
+        };
+  
+        console.log(message)
+  
+        try {
+          console.log('>>>>>making a call<<<<<<<', outgoingMessage)
+          await axios.post("http://localhost:3000/api/conversation", outgoingMessage);
+        } catch (err) {
+          console.log(err);
+        }
+    };
+
+    const handleOutgoingMessage = (event) => {
+      setMessage(event.target.value)
+    }
 
     return (
         <div className={classes.root}>
@@ -50,26 +101,29 @@ export default function MessageBox() {
                 <Grid item xs={12} align="center">
                     <h2 className={classes.header}>Message</h2>
                 </Grid>
-            </Grid>
-            <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Paper elevation={20} className={classes.paper}>
-                    <div className="chatBoxWrapper">
-                        <div className="chatBoxTop">
+                        <div className={classes.messageBoxTop}>
                             <Message />
-                            <Message own={true}/>
-                            <Message />
-                            <Message own={true}/>
-                            <Message />
-                            <Message />
-                            <Message own={true}/>
                             <Message />
                         </div>
-                        <div className="chatBoxBottom">
-                            <textarea className="chatMessageInput" placeholder="write something..."></textarea>
-                            <button className="chatSubmitButton">Send</button>
-                        </div>
-                      </div>
+                        <Divider/>
+                        <form onSubmit={handleSubmit} className={classes.form}>
+                          <CustomTextField 
+                            required
+                            fullWidth
+                            value={message}
+                            onChange={handleOutgoingMessage} 
+                            type='outgoing'
+                            variant='outlined'
+                            label='write something...'
+                          />
+                          <div className={classes.btnWrap}>
+                            <Button type='submit' variant='contained' className={classes.btn}>
+                              Send
+                            </Button>
+                          </div>
+                        </form>
                     </Paper>
                 </Grid>
             </Grid>
