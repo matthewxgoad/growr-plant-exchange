d60bd412c0119e2e829b182880f0903bf1f464fc
Riheel Hamoande <riheel83@gmail.com>
2021-07-18 11:55:01 -0500

    deploy try 10

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  })
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-})
 
 app.use(cors())
 
