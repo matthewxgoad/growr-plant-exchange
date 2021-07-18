f4e7df7036d3043afea3193a86f1ced6e726987f
Riheel Hamoande <riheel83@gmail.com>
2021-07-18 11:04:01 -0500

    deploy try 5

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 app.use(cors())
 
