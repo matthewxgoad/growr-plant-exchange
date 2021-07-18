2fe3e46a58de470d1c4d34fefd612bd1f80c44bb
Riheel Hamoande <riheel83@gmail.com>
2021-07-18 10:28:58 -0500

    deploy try3

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
 app.use(bodyParser.json());
 
 // Define middleware here
@@ -29,6 +31,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
 
 
 
