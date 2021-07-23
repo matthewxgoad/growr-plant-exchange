8fb95df170f09253920d7eb77118cebd36828b7d
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-07-22 19:09:47 -0500

    readmore logic added

--- client/src/components/TradeCard/TradeCard.jsx
+++ client/src/components/TradeCard/TradeCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -7,12 +7,10 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-// import Link from "@material-ui/core/Link";
 import red from "@material-ui/core/colors/red";
 import blue from "@material-ui/core/colors/blue";
 import green from "@material-ui/core/colors/green";
 import moment from "moment";
-import { useEffect } from "react";
 import API from "../../util/API/API";
 
 const tradeBlue = blue[900];
@@ -56,6 +54,7 @@ const useStyles = makeStyles({
 
 export default function TradeCard({ trade, loadTrades }) {
   const classes = useStyles(trade.tradeType);
+  const [readMoreState, setReadMoreState] = useState(false)
 
   let loggedInUserData = JSON.parse(localStorage.getItem("user"));
   const userId = loggedInUserData;
@@ -99,7 +98,9 @@ export default function TradeCard({ trade, loadTrades }) {
         </Typography>
         <br />
         <Typography gutterBottom variant="body2">
-          {trade.description}
+          {readMoreState? trade.description : `${trade.description.substring(0, 50)}...`}
+          <button onClick={() => setReadMoreState(!readMoreState)}>{readMoreState? 'show less':'read more'}</button>
+          {/* {trade.description} */}
         </Typography>
         <br />
         <Typography gutterBottom variant="caption" color="textSecondary">
