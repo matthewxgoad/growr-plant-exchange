b9815f9e703b851ae596363609de036094d2543d
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-07-26 00:27:36 -0500

    transition modal added, data not returning

--- client/src/components/FirstMessage/index.js
+++
@@ -1,3 +0,0 @@
-import FirstMessage from "./FirstMessage";
-
-export default FirstMessage;
--- client/src/components/TradeCard/TradeCard.jsx
+++ client/src/components/TradeCard/TradeCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -7,19 +7,21 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-// import Link from "@material-ui/core/Link";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { Grid, Paper, TextField } from '@material-ui/core';
 import red from "@material-ui/core/colors/red";
 import blue from "@material-ui/core/colors/blue";
 import green from "@material-ui/core/colors/green";
 import moment from "moment";
-import { useEffect } from "react";
 import API from "../../util/API/API";
 
 const tradeBlue = blue[900];
 const tradeGreen = green[700];
 const tradeRed = red[900];
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: 300,
     margin: "10px",
@@ -50,17 +52,41 @@ const useStyles = makeStyles({
   tradeReq: {
     color: tradeRed,
   },
-});
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  grid: {
+    height: 'auto',
+    maxWidth: '50%',
+    margin: "20px"
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    flex: '5.5',
+  },
+  form: {
+    marginTop: '15px'
+  },
+  btnWrap: {
+    margin: '10px 10px 0 0',
+    textAlign: 'right'
+  }
+}));
 
 
 
 export default function TradeCard({ trade, loadTrades }) {
   const classes = useStyles(trade.tradeType);
 
+  const [open, setOpen] = useState(false);
+  const [newConvoState, setNewConvoState] = useState({})
+  const [newMessageState, setNewMessageState] = useState("");
+
   let loggedInUserData = JSON.parse(localStorage.getItem("user"));
   const userId = loggedInUserData;
-
-  console.log( "userId", userId, "creatorId", trade.creator)
 
   const formatedDate = moment(trade.tradeCreated).format("L");
 
@@ -79,6 +105,45 @@ export default function TradeCard({ trade, loadTrades }) {
       return classes.tradeTrade;
     }
   };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newConvo = {
+      senderId: userId,
+      recieverId: trade.creator
+    };
+
+    try {
+      const res = await API.postConversation(newConvo)
+      console.log(res.data)
+      setNewConvoState(res.data)
+    } catch(err){
+      console.log(err)
+    }
+    const message = {
+      sender: userId,
+      text: newMessageState,
+      conversationId: newConvoState._id
+    }
+    try {
+      await API.postMessage(message)
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  const handleOutgoingMessage = (e) => {
+    setNewMessageState(e.target.value)
+  }
 
   return (
     <Card className={classes.root}>
@@ -111,13 +176,56 @@ export default function TradeCard({ trade, loadTrades }) {
       <CardActions className={classes.action}>
         {/* if  userid === trade.creator DELETE else CONTACT */}
         { userId !== trade.creator ? (
-          <Button
-            size="small"
-            color="secondary"
-            href={`/profiles/${trade.creator}`}
-          >
-            CONTACT
-          </Button>
+          <>
+            <Button
+              size="small"
+              color="secondary"
+              onClick={handleOpen}
+            >
+              MESSAGE
+            </Button>
+            <div>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <Grid container className={classes.grid}>
+                    <Grid item xs={12}>
+                      <Paper elevation={20} className={classes.paper}>
+                        <form  onSubmit={handleSubmit} className={classes.form}>
+                          <TextField 
+                            required
+                            fullWidth
+                            multiline
+                            rows={3}
+                            value={newMessageState}
+                            onChange={handleOutgoingMessage} 
+                            type='outgoing'
+                            variant='outlined'
+                            label={`Send a message about ${trade.title} to ${trade.name}`}
+                          />
+                          <div className={classes.btnWrap}>
+                            <Button type='submit' variant='contained' color="primary">
+                              Send
+                            </Button>
+                          </div>
+                        </form>
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                </Fade>
+              </Modal>
+            </div>
+          </>
         ) : (
           <Button
             size="small"
