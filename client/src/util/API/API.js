afee207b90844d8ec1ba646ef8a7cb8634541d32
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-07-28 14:10:15 -0500

    additional axios calls for font end

--- client/src/util/API/API.js
+++ client/src/util/API/API.js
@@ -4,29 +4,35 @@ import axios from "axios";
 
 // eslint-disable-next-line
 export default {
-  // Get all trades in radius by logged in uid
+  // GET all trades in radius by logged in uid
   getTrades: function(id) {
-    // Do we need /API/ here? Probably not.
     return axios.get('/api/users/radius/trades/'+ id);
   },
-  // Get all events in radius by logged in uid
+  // GET all events in radius by logged in uid
   getEvents: function(id) {
     return axios.get('/api/users/radius/events/'+ id);
   },
- // Get all places in radius by logged in uid
+ // GET all places in radius by logged in uid
   getPlaces: function(id) {
     return axios.get('/api/users/radius/places/'+ id);
   },
-  // Get logged in user info by id
+  // GET user by uid
   getUser: function(id) {
     return axios.get('/api/users/profile/'+ id);
   },
+  // DELETE trade data by trade id
   deleteTrade: function(id) {
     return axios.delete('/api/trades/'+ id);
   },
-
-
-  // TO DO New user sign up
+    // DELETE event data by event id
+  deleteEvent: function(id) {
+    return axios.delete('/api/events/'+ id);
+  },
+    // DELETE place data by place id
+  deletePlace: function(id) {
+    return axios.delete('/api/places/'+ id);
+  },
+  // create new user
   createUser: function(formData) {
     return axios.post("/api/users/signup/", formData)
   }
